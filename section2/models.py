192560464d7f62397e46f53f719cd45afae2685a
Kavindu Fernando <kavindu2331@gmail.com>
2022-12-04 11:31:30 +0400

    added the django models

--- section2/models.py
+++ section2/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+
+class Job(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Interviewer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    job = models.ManyToManyField(Job)
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    job = models.ManyToManyField(Job)
+
+
+class Assessor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    job = models.ManyToManyField(Job)
+
+
+class Assessment(models.Model):
+    assessor = models.ForeignKey(Assessor, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+
+
+class QuestionSet(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
